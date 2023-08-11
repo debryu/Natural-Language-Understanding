dd2f02b3d28b3c6df50c0b21f9e000e15813a489
debryu <116577959+debryu@users.noreply.github.com>
2023-08-11 19:25:13 +0200

    Done ex 6

--- 239782_nicola_debole/LAB_06/functions.py
+++ 239782_nicola_debole/LAB_06/functions.py
@@ -1,2 +1,95 @@
 # Add the class of your model only
 # Here is where you define the architecture of your model using pytorch
+# Spacy version 
+from nltk.corpus import dependency_treebank
+from nltk.parse.dependencygraph import DependencyGraph
+from spacy.tokenizer import Tokenizer
+import spacy 
+import spacy_conll
+import stanza
+import spacy_stanza
+
+from nltk.parse import DependencyEvaluator
+
+
+
+def evaluate_parses(parses):
+    de = DependencyEvaluator(parses, dependency_treebank.parsed_sents()[-100:])
+    las, uas = de.eval()
+    return las, uas
+
+def create_dataset():
+    treebank_sentences = dependency_treebank.sents()
+    last_100_sentences = treebank_sentences[-100:]
+    return last_100_sentences
+
+def preprocess_data(list_of_list_of_words):
+    sentences = []
+    # Since the words are stored in a list of lists, we need to join all the words in a sentence
+    for list_of_words in list_of_list_of_words:
+        sentence = ' '.join(list_of_words)
+        sentences.append(sentence)
+    return sentences
+
+
+# Load the spacy model
+spacy_model = spacy.load("en_core_web_sm")
+
+# Set up the conll formatter 
+spacy_config = {"ext_names": {"conll_pd": "pandas"},
+        "conversion_maps": {"DEPREL": {"nsubj": "subj"}}}
+
+# Add the formatter to the pipeline
+spacy_model.add_pipe("conll_formatter", config=spacy_config, last=True)
+# Split by white space
+spacy_model.tokenizer = Tokenizer(spacy_model.vocab) 
+
+def spacy_parsing(sentence):
+    # Parse the sentence
+    doc = spacy_model(sentence)
+
+    # Convert doc to a pandas object
+    df = doc._.pandas
+
+    # Select the columns accoroding to Malt-Tab format
+    tmp = df[["FORM", 'XPOS', 'HEAD', 'DEPREL']].to_string(header=False, index=False)
+    tmp_as_df = df[["FORM", 'XPOS', 'HEAD', 'DEPREL']]
+    
+    # See the outcome
+    #print(tmp)
+
+    # Get finally our the DepencecyGraph
+    dp = DependencyGraph(tmp)
+    #print('Tree:')
+    #dp.tree().pretty_print(unicodelines=True, nodedist=4)
+
+    return tmp_as_df, dp
+
+
+# Set up the conll formatter 
+#tokenize_pretokenized used to tokenize by whitespace 
+stanza_model = spacy_stanza.load_pipeline("en", verbose=False, tokenize_pretokenized=True)
+stanza_config = {"ext_names": {"conll_pd": "pandas"},
+          "conversion_maps": {"DEPREL": {"nsubj": "subj", "root":"ROOT"}}}
+
+# Add the formatter to the pipeline
+stanza_model.add_pipe("conll_formatter", config=stanza_config, last=True)
+
+def stanza_parsing(sentence):
+    # Parse the sentence
+    doc = stanza_model(sentence)
+    # Convert doc to a pandas object
+    df = doc._.pandas
+    #print(df)
+    # Select the columns accoroding to Malt-Tab format
+    tmp = df[["FORM", 'XPOS', 'HEAD', 'DEPREL']].to_string(header=False, index=False)
+    tmp_as_df = df[["FORM", 'XPOS', 'HEAD', 'DEPREL']]
+    # See the outcome
+    #print(tmp)
+
+    # Get finally our the DepencecyGraph
+    dp = DependencyGraph(tmp)
+    #print('Tree:')
+    #dp.tree().pretty_print(unicodelines=True, nodedist=4)
+
+    return tmp_as_df, dp
--- 239782_nicola_debole/LAB_06/main.py
+++ 239782_nicola_debole/LAB_06/main.py
@@ -3,7 +3,36 @@
 
 # Import everything from functions.py file
 from functions import *
+# Import tqdm for the progress bar
+from tqdm import tqdm
+import pandas as pd
 
 if __name__ == "__main__":
     #Wrtite the code to load the datasets and to run your functions
     # Print the results
+    data = create_dataset()
+    
+    print('Comparing Spacy and Stanza to see if the dependency tags are the same:')
+    spacy_dependency_graphs = []
+    stanza_dependency_graphs = []
+    for sentence in tqdm(preprocess_data(data)):
+        
+        spacy_dataframe, spacy_depgraph = spacy_parsing(sentence)
+        spacy_dependency_graphs.append(spacy_depgraph)
+        stanza_dataframe, stanza_depgraph = stanza_parsing(sentence)
+        stanza_dependency_graphs.append(stanza_depgraph)
+        results = pd.concat([spacy_dataframe[['FORM','DEPREL']], stanza_dataframe[['DEPREL']]], axis=1)
+        new_headers = ['FORM', 'Spacy', 'Stanza']
+        results.columns = new_headers
+        print(results)
+
+    print('--------------------------------[EVALUATION]--------------------------------')
+    print('Evaluating Spacy:')
+    spacy_las, spacy_uas = evaluate_parses(spacy_dependency_graphs)
+    print('LAS: ', spacy_las)
+    print('UAS: ', spacy_uas)
+    print('Evaluating Stanza:')
+    stanza_las, stanza_uas = evaluate_parses(stanza_dependency_graphs)
+    print('LAS: ', stanza_las)
+    print('UAS: ', stanza_uas)
+    
