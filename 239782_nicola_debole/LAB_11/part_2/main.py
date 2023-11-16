827ed1e9e87f7b4088fa8d5fff53573d06731590
debryu <116577959+debryu@users.noreply.github.com>
2023-11-16 15:02:41 +0100

    added dataset and dataloading

--- 239782_nicola_debole/LAB_11/part_2/main.py
+++ 239782_nicola_debole/LAB_11/part_2/main.py
@@ -3,7 +3,12 @@
 
 # Import everything from functions.py file
 from functions import *
+from utils import *
 
 if __name__ == "__main__":
     #Wrtite the code to load the datasets and to run your functions
     # Print the results
+    train_ds = read_data('dataset/LAB11_part2/laptop14_train.txt')
+    test_ds = read_data('dataset/LAB11_part2/laptop14_test.txt')
+
+    
--- 239782_nicola_debole/LAB_11/part_2/utils.py
+++ 239782_nicola_debole/LAB_11/part_2/utils.py
@@ -1,1 +1,57 @@
 # Add functions or classes used for data loading and preprocessing
+import string
+
+def read_data(path):
+    """
+    read data from the specified path
+    :param path: path of dataset
+    :return:
+    """
+    dataset = []
+    with open(path, encoding='UTF-8') as fp:
+        for line in fp:
+            print(line)
+            record = {}
+            sent, tag_string = line.strip().split('####')
+            record['sentence'] = sent
+            word_tag_pairs = tag_string.split(' ')
+            # tag sequence for targeted sentiment
+            ts_tags = []
+            # tag sequence for opinion target extraction
+            ote_tags = []
+            # word sequence
+            words = []
+            for item in word_tag_pairs:
+                # valid label is: O, T-POS, T-NEG, T-NEU
+                eles = item.split('=')
+                if len(eles) == 2:
+                    word, tag = eles
+                elif len(eles) > 2:
+                    tag = eles[-1]
+                    word = (len(eles) - 2) * "="
+                if word not in string.punctuation:
+                    # lowercase the words
+                    words.append(word.lower())
+                else:
+                    # replace punctuations with a special token
+                    words.append('PUNCT')
+                if tag == 'O':
+                    ote_tags.append('O')
+                    ts_tags.append('O')
+                elif tag == 'T-POS':
+                    ote_tags.append('T')
+                    ts_tags.append('T-POS')
+                elif tag == 'T-NEG':
+                    ote_tags.append('T')
+                    ts_tags.append('T-NEG')
+                elif tag == 'T-NEU':
+                    ote_tags.append('T')
+                    ts_tags.append('T-NEU')
+                else:
+                    raise Exception('Invalid tag %s!!!' % tag)
+            record['words'] = words.copy()
+            record['ote_raw_tags'] = ote_tags.copy()
+            record['ts_raw_tags'] = ts_tags.copy()
+            dataset.append(record)
+    print("Obtain %s records from %s" % (len(dataset), path))
+    return dataset
